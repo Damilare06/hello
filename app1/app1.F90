7f1d9e137b86f412ebd10a857d3e912c2992bf17
Damilare <adesoa@rpi.edu>
2020-02-10 17:16:08 -0500

    2D  array in serial complete

--- app1/app1.F90
+++ app1/app1.F90
@@ -1,35 +1,24 @@
 program app1
-  use mpi
-  use adios2
-  use functions
+    use mpi
+    use adios2
+    use functions
 
-  implicit none
+    implicit none
 
-  real, dimension(:), allocatable :: field
-  real, dimension(0:3,0:1) :: density
-  !real :: density(4,2) = reshape((/1,2,3,4,5,6,7,8/), (/4,2/))
-  !REAL,DIMENSION(4,2) :: density = RESHAPE([1,2,3,4,5,6,7,8],[4,2])
+    real, dimension(:), allocatable :: field
+    !resize to a 4 by 2 matrix
+    real :: density(4,2) = reshape((/26,22,35,12,5,67,39,17/), (/4,2/))
 
-  integer :: inx, irank, isize, i , comm
-  integer :: ierr = 0
-  ! Launch MPI
-  call MPI_Init(ierr)
-  call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
-  call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
+    integer :: inx, irank, isize 
+    integer :: ierr = 0
+    ! Launch MPI
+    call MPI_Init(ierr)
+    call MPI_Comm_rank(MPI_COMM_WORLD, irank, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, isize, ierr)
 
-  ! Density variables
-density(0,0)= 1.0
-density(1,0)= 2.0
-density(2,0)= 3.0
-density(3,0)= 4.0
-density(0,1)= 5.0
-density(1,1)= 6.0
-density(2,1)= 7.0
-density(3,1)= 8.0
+    call write_density(density, ierr)
 
-  call write_density(density, ierr)
-
-  print *, 'app1_f done'
-  call MPI_Finalize(ierr)
+    print *, 'app1_f done'
+    call MPI_Finalize(ierr)
 
 end program app1
--- app2/app2_2d.cc
+++ app2/app2_2d.cc
@@ -5,31 +5,29 @@
 #include<mpi.h>
 #include<numeric>
 
-using twoD_vec = std::vector<std::vector<float>>; 
 void read_density(float * & foo);
-void write_field(const twoD_vec &field, int rank, int size);
 
 int main(int argc, char *argv[])
 {
   int rank , size, step = 0;
-  twoD_vec density = {{0.0},{0.0}};
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);   
   float * dens = NULL;
   read_density(dens);
-  delete[] dens;
 
   MPI_Barrier(MPI_COMM_WORLD);
 
-/*  if( !rank )
+  if(!rank )
   {
-    assert (density[0][0] == (float)0);
-    std::cout << "The asserted first density value is " << density[0][0] << "\n";
+  //  assert (density[0][0] == (float)0);
+    std::cout << "The first density value is " << dens[0] << "\n";
+    std::cout << "The 4th density value is " << dens[3] << "\n";
     std::cout << "xgc proxy done\n";
-  }*/
+  }
 
+  delete[] dens;
   MPI_Finalize();
   return 0;
 }
@@ -48,30 +46,20 @@ void read_density(float * &dens)
 
     adios2::Engine Reader = IO.Open("dens.bp", adios2::Mode::Read);
     Reader.BeginStep();
-    if(Reader) std::cout << "1.0" << std::endl;
     adios2::Variable<float> cdens = IO.InquireVariable<float>("gdens");
-    if(cdens) std::cerr << "1.1" << std::endl;
     auto height = cdens.Shape()[0];
-    std::cerr << "1.2" << std::endl;
     auto width = cdens.Shape()[1];
     std::cout << "Incoming variable is of size " << height << " by "<< width << std::endl;
 
 
     const adios2::Dims start{0, 0};
-    std::cerr << "1.3" << std::endl;
     const adios2::Dims count{height, width};
-    std::cerr << "1.4" << std::endl;
     const adios2::Box<adios2::Dims> sel(start, count);
-    std::cerr << "1.5" << std::endl;
     dens = new float[height * width];
-    std::cerr << "1.6" << std::endl;
 
     cdens.SetSelection(sel);
-    std::cerr << "1.7" << std::endl;
     Reader.Get<float>(cdens, dens);
-    std::cerr << "1.8" << std::endl;
     Reader.EndStep();
-    std::cerr << "1.9" << std::endl;
 
     Reader.Close();
     std::cout << " done reading\n";
